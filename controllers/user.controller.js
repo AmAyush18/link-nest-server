00d0600c2551f3cd5e1ba618145ebcb21551c289
Ayush Dwivedi <amayush0818@gmail.com>
2024-01-10 23:58:55 +0530

    login user api created

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -41,3 +41,34 @@ export const registrationUser = CatchAsyncError(async (req, res) => {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
+
+
+export const loginUser = CatchAsyncError(async (req, res) => {
+    try {
+      const { email, password } = req.body;
+  
+      // Check if the email exists
+      const user = await userModel.findOne({ email });
+  
+      if (!user) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+      }
+  
+      // Check if the provided password is correct
+      const passwordMatch = await bcrypt.compare(password, user.password);
+  
+      if (!passwordMatch) {
+        return res.status(401).json({ error: 'Incorrect Password' });
+      }
+  
+      // Generate a JWT token
+      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+        expiresIn: '3h', // Token expiration time
+      });
+  
+      res.json({ message: 'Login successful', token });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  })
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { registrationUser } from '../controllers/user.controller.js';
+import { registrationUser, loginUser } from '../controllers/user.controller.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/registration', registrationUser);
+
+userRouter.post('/login', loginUser);
 
 export default userRouter;
